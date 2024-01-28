794d97963b9eb7a8e8ee81c06fefa752996724e3
HiraniPJ <pooja.hirani93@gmail.com>
2024-01-28 15:43:01 +0000

    changed div to nav

---
+++ Assets/script.js
@@ -0,0 +1,11 @@
+function menuToggle() 
+{
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") 
+    {
+        x.className += " responsive";
+    } else 
+    {
+        x.className = "topnav";
+    }
+}
