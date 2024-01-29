79397f9224d15bc7fb7a73e3e603828460ee9e94
HiraniPJ <pooja.hirani93@gmail.com>
2024-01-29 23:58:50 +0000

    script added for the gallery

--- Assets/script.js
+++ Assets/script.js
@@ -11,3 +11,11 @@ function menuToggle()
         x.style.display = "block";
     }
 }
+
+function myFunction(imgs) {
+    var expandImg = document.getElementById("expandedImg");
+    var imgText = document.getElementById("imgtext"); 
+    expandImg.src = imgs.src;
+    imgText.innerHTML = imgs.alt;
+    expandImg.parentElement.style.display = "block";
+  }
