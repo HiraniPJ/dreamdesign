53c25c82bfa97ec36d0ed7b78a26fa80433cc965
HiraniPJ <pooja.hirani93@gmail.com>
2024-01-30 00:13:34 +0000

    images style added

--- Assets/script.js
+++ Assets/script.js
@@ -14,8 +14,6 @@ function menuToggle()
 
 function myFunction(imgs) {
     var expandImg = document.getElementById("expandedImg");
-    var imgText = document.getElementById("imgtext"); 
     expandImg.src = imgs.src;
-    imgText.innerHTML = imgs.alt;
     expandImg.parentElement.style.display = "block";
   }
